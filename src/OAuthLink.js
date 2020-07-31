f8f83d4a8b6e8d92315038f4e936705fd9d5f1ae
Victor Song <vms2@rice.edu>
2020-07-30 23:58:28 -0500

    Initial setup of onboarding and OAuth reception

---
+++ src/OAuthLink.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import {
+    SQUARE_CONNECTION_BASE_URL,
+    SQUARE_CLIENT_ID,
+    HEDWIG_VENDOR_PERMISSIONS,
+} from "./config";
+
+const SQUARE_AUTH_PAGE =
+    SQUARE_CONNECTION_BASE_URL +
+    "oauth2/authorize?client_id=" +
+    SQUARE_CLIENT_ID +
+    "&scope=" +
+    HEDWIG_VENDOR_PERMISSIONS.join("+") +
+    "&state=82201dd8d83d23cc8a48caf52ba4f4fb";
+
+function OAuthLink() {
+    return (
+        <div>
+            <a href={SQUARE_AUTH_PAGE}>
+                Click here to grant Hedwig permission to access your Square
+                account!
+            </a>
+        </div>
+    );
+}
+
+export default OAuthLink;
---
+++ src/ReceiveOAuth.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Link } from "react-router";
+
+import { OBTAIN_TOKEN_WORKER } from "./config";
+
+function ReceiveOAuth() {
+    let result;
+
+    if (window.location.search === "") {
+        result = (
+            <div>
+                Sorry, you must <Link to="/onboard">sign up here</Link> with
+                your Square account to see this page.
+            </div>
+        );
+    }
+
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (searchParams.has("error")) {
+        result = (
+            <div>
+                Sorry! Signup failed with code ${searchParams.get("error")} for
+                reason ${searchParams.get("error_description")}. If you believe
+                this is a mistake,{" "}
+                <Link to="/onboard">please sign up again here</Link>.
+            </div>
+        );
+    } else if (searchParams.has("code")) {
+        const data = {
+            accessCode: searchParams.get("code")
+        }
+        const workerResponse = await fetch(OBTAIN_TOKEN_WORKER, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (workerResponse.status === 200) {
+            result = (
+                <div>
+                    Thanks for signing up with Hedwig! Your vendor page will be
+                    ready shortly.
+                </div>
+            );
+        } else if (workerResponse.status === 405) {
+            result = (
+                <div>
+                    Please don't attempt to use anything other than POST. Go back to <Link to="/onboard">the onboarding endpoint</Link>.
+                </div>
+            )
+        } else {
+            result = (
+                <div>
+                    Your access code is invalid. Please attempt <Link to="/onboard">reauthorization here</Link>.
+                </div>
+            )
+        }
+    } else {
+        result = (
+            <div>
+                Sorry, but an unknown error has occurred. Please go{" "}
+                <Link to="/onboard">here</Link> to sign up again.
+            </div>
+        );
+    }
+
+    return result;
+}
+
+export default ReceiveOAuth;
